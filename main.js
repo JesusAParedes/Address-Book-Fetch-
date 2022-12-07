b7228c57973379c6e6a0d9996b0aa9643d859a4d
JesusAParedes <paredesjesus93@gmail.com>
2022-12-06 22:09:12 -0600

    got one picture to show up and the names to show on the page

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
-const fetchData = () => {
-    fetch(https://randomuser.me/api/)
+let arrayofUsers;
+
+window.onload = function() {
+    grabData();
 }
+
+const grabData = () => {
+    fetch('https://randomuser.me/api/?results=2')
+    .then(res => res.json())
+    .then(posts => arrayofUsers = posts.results)
+    
+}
+
+const listUsers = () => {
+    const list = document.getElementById('user');
+    const img = document.querySelector("img");
+    arrayofUsers.map(user => {
+        img.src = `${user.picture.thumbnail}`;
+        const text = document.createTextNode(`First Name: ${user.name.first} Last Name: ${user.name.last}` 
+        ,)
+        const li = document.createElement('li');
+        li.appendChild(text);
+        list.append(li);})
+        
+}
+
+
