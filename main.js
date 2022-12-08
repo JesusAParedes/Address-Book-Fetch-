271dae478b9fa9453912c7409eddd315b5013869
JesusAParedes <paredesjesus93@gmail.com>
2022-12-07 22:44:53 -0600

    dob, location, and cell are able to update on to address book when a button is created. Just need to make it look better if needed

--- main.js
+++ main.js
@@ -13,19 +13,48 @@ const grabData = () => {
 
 const listUsers = () => {
     const list = document.getElementById('user');
-    const img = new Image();
-
+    arrayofUsers.map(otherInfo => {
+        const newText = document.createTextNode(`DOB: ${user.dob} Address: ${user.location}`);
+    })
+    
     arrayofUsers.map(user => {
-        const text = document.createTextNode(`First Name: ${user.name.first} Last Name: ${user.name.last} ${user.picture.thumbnail}`);
+        const img = new Image();
+        let text = document.createTextNode(`First Name: ${user.name.first} Last Name: ${user.name.last}`);
+        const button = document.createElement('button');
+        button.innerHTML = "More Info About Me!";
         img.src = user.picture.thumbnail;
-
+        
         const li = document.createElement('li');
+        button.addEventListener("click", extraInfo)
         li.appendChild(text);
-        list.append(`${img}`);
+        list.append(img);
+        list.append(button);
         list.append(li);
     }
     )
         
+}
+
+
+const extraInfo = () => {
+    const list = document.getElementById('user');
+    arrayofUsers.map(user => {
+        dob = document.createTextNode(`DOB: ${user.dob.date}`);
+
+        loc = document.createTextNode(`Location: ${user.location.street.number} ${user.location.street.name} ${user.location.city}, ${user.location.country}`);
+
+        cell = document.createTextNode(`Cell: ${user.cell}`);
+    })
+    const li = document.createElement('li');
+    console.log(dob);
+    console.log(location);
+    console.log(cell);
+    li.appendChild(dob);
+    li.appendChild(loc);
+    li.appendChild(cell);
+    list.append(li);
+    list.append(li);
+    
 }
 
 
